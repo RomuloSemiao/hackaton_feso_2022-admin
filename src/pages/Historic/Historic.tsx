334fcfcae73819269db65dce0bd64a183fde5c59
Luiz Felipe Zimbrão Gomes <felipe.zimbrao@agenciam3.com>
2022-10-23 02:25:46 -0300

    integração com historico

--- src/pages/Historic/Historic.tsx
+++ src/pages/Historic/Historic.tsx
@@ -1,4 +1,10 @@
 const Historic = () => {
+    fetch("https://sodafront-ehealth-backend.herokuapp.com/appointments")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log({json})
+      })
+
   return (
     <>Historic</>
   )
